43cfe0348a4bda6086467840c344ca2ece0e31f7
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-06-01 16:51:35 -0300

    configObjetJson

--- src/main/java/com/deusley/paymentservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/deusley/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,17 +2,29 @@ package com.deusley.paymentservice.service.impl;
 
 import com.deusley.paymentservice.model.Payment;
 import com.deusley.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Service
 @Log4j2
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL :: Pagamento Recebido {}", payment);
 
+        log.info("Pagamento Recebido {}", payment);
+        Thread.sleep(1000);
+        log.info("enviando o pagamento");
+
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
